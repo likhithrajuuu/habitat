e1993fdffeedf3a6e0fc29ed78d8b5f16b97db6b
Likhith Raju P <98330732+likhithrajuuu@users.noreply.github.com>
2025-12-24 23:55:52 +0530

    Add monitoring stack and refactor Movie entity
    Introduced Prometheus and Grafana monitoring with Docker Compose and configuration files. Refactored the Movie entity for cleaner code, improved relationship annotations, and added missing getter/setter for moviePoster. Updated application properties to expose Prometheus metrics endpoints.

--- habitat-backend/src/main/java/com/project/habitat/model/Movie.java
+++ habitat-backend/src/main/java/com/project/habitat/model/Movie.java
@@ -1,12 +1,9 @@
 package com.project.habitat.model;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(schema = "dev", name = "movies")
@@ -28,164 +25,79 @@ public class Movie {
 
     private Double avgRating;
 
-    @ManyToMany
+    private String moviePoster;
+
+    /* ---------- RELATIONSHIPS ---------- */
+
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
-            name = "movie_genres",
-            schema = "dev",
-            joinColumns = @JoinColumn(name = "movie_id"),
-            inverseJoinColumns = @JoinColumn(name = "genre_id")
+        name = "movie_genres",
+        schema = "dev",
+        joinColumns = @JoinColumn(name = "movie_id"),
+        inverseJoinColumns = @JoinColumn(name = "genre_id")
     )
-	@JsonManagedReference
     private Set<Genre> genres;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
-            name = "movie_formats",
-            schema = "dev",
-            joinColumns = @JoinColumn(name = "movie_id"),
-            inverseJoinColumns = @JoinColumn(name = "format_id")
+        name = "movie_formats",
+        schema = "dev",
+        joinColumns = @JoinColumn(name = "movie_id"),
+        inverseJoinColumns = @JoinColumn(name = "format_id")
     )
     private Set<Format> formats;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
-            name = "movie_languages",
-            schema = "dev",
-            joinColumns = @JoinColumn(name = "movie_id"),
-            inverseJoinColumns = @JoinColumn(name = "language_id")
+        name = "movie_languages",
+        schema = "dev",
+        joinColumns = @JoinColumn(name = "movie_id"),
+        inverseJoinColumns = @JoinColumn(name = "language_id")
     )
     private Set<Language> languages;
 
-    @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "movie", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Rating> ratings;
-    
-	private String moviePoster;
 
-    public Movie() {
-    	
-    }
+    /* ---------- CONSTRUCTORS ---------- */
 
-	public Movie(Long movieId, String movieName, String movieDescription, Integer durationMinutes, String certificate,
-			LocalDate releaseDate, Double avgRating, Set<Genre> genres, Set<Format> formats, Set<Language> languages,
-			List<Rating> ratings, String moviePoster) {
-		super();
-		this.movieId = movieId;
-		this.movieName = movieName;
-		this.movieDescription = movieDescription;
-		this.durationMinutes = durationMinutes;
-		this.certificate = certificate;
-		this.releaseDate = releaseDate;
-		this.avgRating = avgRating;
-		this.genres = genres;
-		this.formats = formats;
-		this.languages = languages;
-		this.ratings = ratings;
-		this.moviePoster = moviePoster;
-	}
+    public Movie() {}
 
-	public Long getMovieId() {
-		return movieId;
-	}
+    /* ---------- GETTERS & SETTERS ---------- */
 
-	public void setMovieId(Long movieId) {
-		this.movieId = movieId;
-	}
+    public Long getMovieId() { return movieId; }
+    public void setMovieId(Long movieId) { this.movieId = movieId; }
 
-	public String getMovieName() {
-		return movieName;
-	}
+    public String getMovieName() { return movieName; }
+    public void setMovieName(String movieName) { this.movieName = movieName; }
 
-	public void setMovieName(String movieName) {
-		this.movieName = movieName;
-	}
+    public String getMovieDescription() { return movieDescription; }
+    public void setMovieDescription(String movieDescription) { this.movieDescription = movieDescription; }
 
-	public String getMovieDescription() {
-		return movieDescription;
-	}
+    public Integer getDurationMinutes() { return durationMinutes; }
+    public void setDurationMinutes(Integer durationMinutes) { this.durationMinutes = durationMinutes; }
 
-	public void setMovieDescription(String movieDescription) {
-		this.movieDescription = movieDescription;
-	}
+    public String getCertificate() { return certificate; }
+    public void setCertificate(String certificate) { this.certificate = certificate; }
 
-	public Integer getDurationMinutes() {
-		return durationMinutes;
-	}
+    public LocalDate getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(LocalDate releaseDate) { this.releaseDate = releaseDate; }
 
-	public void setDurationMinutes(Integer durationMinutes) {
-		this.durationMinutes = durationMinutes;
-	}
+    public Double getAvgRating() { return avgRating; }
+    public void setAvgRating(Double avgRating) { this.avgRating = avgRating; }
 
-	public String getCertificate() {
-		return certificate;
-	}
+    public String getMoviePoster() { return moviePoster; }
+    public void setMoviePoster(String moviePoster) { this.moviePoster = moviePoster; }
 
-	public void setCertificate(String certificate) {
-		this.certificate = certificate;
-	}
+    public Set<Genre> getGenres() { return genres; }
+    public void setGenres(Set<Genre> genres) { this.genres = genres; }
 
-	public LocalDate getReleaseDate() {
-		return releaseDate;
-	}
+    public Set<Format> getFormats() { return formats; }
+    public void setFormats(Set<Format> formats) { this.formats = formats; }
 
-	public void setReleaseDate(LocalDate releaseDate) {
-		this.releaseDate = releaseDate;
-	}
+    public Set<Language> getLanguages() { return languages; }
+    public void setLanguages(Set<Language> languages) { this.languages = languages; }
 
-	public Double getAvgRating() {
-		return avgRating;
-	}
-
-	public void setAvgRating(Double avgRating) {
-		this.avgRating = avgRating;
-	}
-
-	public Set<Genre> getGenres() {
-		return genres;
-	}
-
-	public void setGenres(Set<Genre> genres) {
-		this.genres = genres;
-	}
-
-	public Set<Format> getFormats() {
-		return formats;
-	}
-
-	public void setFormats(Set<Format> formats) {
-		this.formats = formats;
-	}
-
-	public Set<Language> getLanguages() {
-		return languages;
-	}
-
-	public void setLanguages(Set<Language> languages) {
-		this.languages = languages;
-	}
-
-	public List<Rating> getRatings() {
-		return ratings;
-	}
-
-	public void setRatings(List<Rating> ratings) {
-		this.ratings = ratings;
-	}
-
-	public String getMoviePoster(String moviePoster){
-		return moviePoster;
-	}
-
-	public void setMoviePoster(String moviePoster){
-		this.moviePoster = moviePoster;
-	}
-
-	@Override
-	public String toString() {
-		return "Movie [movieId=" + movieId + ", movieName=" + movieName + ", movieDescription=" + movieDescription
-				+ ", durationMinutes=" + durationMinutes + ", certificate=" + certificate + ", releaseDate="
-				+ releaseDate + ", avgRating=" + avgRating + ", genres=" + genres + ", formats=" + formats
-				+ ", languages=" + languages + ", ratings=" + ratings + ", moviePoster=" + "]";
-	}
-    
-    
+    public List<Rating> getRatings() { return ratings; }
+    public void setRatings(List<Rating> ratings) { this.ratings = ratings; }
 }
